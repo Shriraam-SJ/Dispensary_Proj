8c69c07078155114e6edae74ce012c3d085d25e1
Shriraam-SJ <sharanshriraam@student.tce.edu>
2025-09-09 15:07:08 +0530

    Update server.js

--- patient-diagnosis/src/components/backend/server.js
+++ patient-diagnosis/src/components/backend/server.js
@@ -15,16 +15,13 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-const allowedOrigins = [
-  'http://localhost:5173',
-  'https://nurseinventory.vercel.app',
-  'https://dispensary-proj.onrender.com'// <-- ADD YOUR LIVE FRONTEND URL HERE
-];
+?
 app.use(cors({
-  origin: function(origin, callback) {
-    if (!origin) return callback(null, true);
-    callback(new Error('Not allowed by CORS'));
-  },
+  // origin: function(origin, callback) {
+  //   if (!origin) return callback(null, true);
+  //   callback(new Error('Not allowed by CORS'));
+  // },
+  origin: '*',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
 }));
